2c6006af47439f0de944f70a14739e299cf5c576
wilzh40 <wilzh40@gmail.com>
2014-10-05 19:06:39 -0700

    Used router.js instead of app.get

--- server.js
+++ server.js
@@ -1,35 +1,110 @@
 var express  = require('express');
-	var app      = express(); 								// create our app w/ express
-	var mongoose = require('mongoose'); 					// mongoose for mongodb
-	var morgan = require('morgan'); 			// log requests to the console (express4)
-	var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
-	var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var morgan = require('morgan'); 			// log requests to the console (express4)
+var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-	// configuration =================
+// configuration =================
 
-	mongoose.connect('mongodb://a:b@proximus.modulusmongo.net:27017/ziva7qEx'); 	// connect to mongoDB database on modulus.io
+mongoose.connect('mongodb://a:b@proximus.modulusmongo.net:27017/ziva7qEx'); 	// connect to mongoDB database on modulus.io
 
-	app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
-	app.use(morgan('dev')); 										// log every request to the console
-	app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
-	app.use(bodyParser.json()); 									// parse application/json
-	app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-	app.use(methodOverride());
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 										// log every request to the console
+/*
+app.use(bodyParser.urlencoded({'extended': 'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json*/
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use(methodOverride());
 
 	// listen (start app with node server.js) ======================================
-	app.listen(8181);
-	console.log("App listening on 8181")
+var router = express.Router(); 		
+
 
 
 // Models
 
-var Api = mongoose.model('Todo', {
+var Api = mongoose.model('Api', {
 		title : String,
 		link : String,
 		imageURL : String
 	});
-var Mashup = mongoose.model('Mashup', {
-		api1 : String,
-		api2 : String,
-		upvotes : Number
+var Mashup = require('./models/mashup');
+
+
+// Routing
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Routing...');
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+app.get('/api/apis', function (req, res) {
+
+	Api.find(function (err, apis) {
+		if (err) {
+			res.send (err)
+        }
+		res.json(apis); 
 	});
+});
+
+app.post('/api/apis', function (req, res){
+	Api.create({
+		title : req.body.text,
+		done : false
+	}, function(err, Api) {
+		if (err)
+			res.send(err);
+        res.json(Api)
+		// get and return all the todos after you create another
+		Api.find(function(err, apis) {
+			if (err)
+				res.send(err)
+			res.json(apis);
+		});
+	});
+
+});
+
+
+router.route('/mashups')
+	.post(function(req,res) {
+		var mashup = new Mashup();
+		mashup.name = req.body.name;
+		mashup.save(function(err) {
+			if (err) {
+				res.send(err);
+			};
+			res.json(mashup);
+		});
+	})
+	.get(function(req,res) {
+		Mashup.find(function(err,mashups) {
+			if (err)
+				res.send(err);
+			res.json(mashups);
+		});
+	});
+
+router.route('/mashups/:mashup_id')
+	.get(function(req,res) {
+		Bear.findById(req.params.bear_id, function(err,bear) {
+			if (err)
+				res.send(err);
+			res.json(bear);
+		})
+	})
+
+
+// Default
+app.get('/', function(req, res) {
+		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
+app.use('/api', router);
+app.listen(8282);
+console.log("App listening on 8181")
